2008e5d4a8d7025f0287fe0adf4ba31396a680e0
SerhiiRavliuk <serhii.ravliuk@gmail.com>
2024-09-12 13:32:27 +0300

    homework_14

--- Lesson_14/iniziallization_student.py
+++ Lesson_14/iniziallization_student.py
@@ -1,12 +1,8 @@
 from Lesson_14.student import Student
 
 student_Serhii_Ravliuk = Student('Serhii', 'Ravliuk', 30, 84.5)
-
 print(student_Serhii_Ravliuk)
 
-# Змінюємо середній бал студента
 student_Serhii_Ravliuk.change_average_score(90.0)
-
-# Виводимо оновлену інформацію про студента
 print(student_Serhii_Ravliuk)
 
