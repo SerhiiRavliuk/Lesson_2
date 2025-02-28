672cf875014d15767b7ef5a76d2034ccb0a653fd
SerhiiRavliuk <serhii.ravliuk@gmail.com>
2025-02-28 13:20:01 +0200

    added full test path 9

--- lesson_28/tests/test_automation.py
+++ lesson_28/tests/test_automation.py
@@ -25,27 +25,27 @@ class TestAutomation:
 
         time.sleep(1)
 
-    @allure.story("Logged out")
-    def test_that_user_can_logged_out(self, web_driver_creation):
-        self.main_page = MainPage(web_driver_creation)
-        self.main_page.logged_out()
-        self.main_page.verify_that_user_logged_out()
-
-    @allure.story("Go to signup page and signup")
-    def test_navigate_to_sign_in_page_and_sign_in(self, web_driver_creation):
-        self.main_page = MainPage(web_driver_creation)
-        self.main_page.go_to_signup_and_login_page()
-        self.signup_and_login_page = SignUpAndLoginPage(web_driver_creation)
-        self.signup_and_login_page.fill_sign_up_fields_and_sign_up(TestData.USER_VALID_NAME, TestData.USER_VALID_EMAIL)
-        self.signup_page = SignUpPage(web_driver_creation)
-        self.signup_page.verify_that_user_is_placed_on_sign_up_page()
-
-
-    @allure.story("Test for issue vizualization")
-    @allure.issue('https://github.com/DartBrovsky/aqa_python_hillel/blob/main/lesson_30/test_initial.py',"test allure issue with link on github")
-    @pytest.mark.skip
-    def test_allure_issue_with_link_on_github(self):
-        assert False
+    # @allure.story("Logged out")
+    # def test_that_user_can_logged_out(self, web_driver_creation):
+    #     self.main_page = MainPage(web_driver_creation)
+    #     self.main_page.logged_out()
+    #     self.main_page.verify_that_user_logged_out()
+    #
+    # @allure.story("Go to signup page and signup")
+    # def test_navigate_to_sign_in_page_and_sign_in(self, web_driver_creation):
+    #     self.main_page = MainPage(web_driver_creation)
+    #     self.main_page.go_to_signup_and_login_page()
+    #     self.signup_and_login_page = SignUpAndLoginPage(web_driver_creation)
+    #     self.signup_and_login_page.fill_sign_up_fields_and_sign_up(TestData.USER_VALID_NAME, TestData.USER_VALID_EMAIL)
+    #     self.signup_page = SignUpPage(web_driver_creation)
+    #     self.signup_page.verify_that_user_is_placed_on_sign_up_page()
+    #
+    #
+    # @allure.story("Test for issue vizualization")
+    # @allure.issue('https://github.com/DartBrovsky/aqa_python_hillel/blob/main/lesson_30/test_initial.py',"test allure issue with link on github")
+    # @pytest.mark.skip
+    # def test_allure_issue_with_link_on_github(self):
+    #     assert False
 
 
 
